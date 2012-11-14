b4a0a978b129bea499273142da0db5c707bb3ef5
steviee <steviee@me.com>
2012-11-14 17:02:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtsWeb::Application.config.secret_token = '8be8aba5ee02b056e8d84cfdadfee811224c2dbe7caeb5233414e2323af4692ab4c995905e61c3cee4044eda0b468e98d35913087b27739578e7323888491209'
