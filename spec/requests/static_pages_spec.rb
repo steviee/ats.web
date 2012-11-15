ce5675824c8532711a968564d0bf2529de32c18a
steviee <steviee@me.com>
2012-11-15 14:59:53 +0100

    Added simple user model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,52 +3,48 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before { visit root_path } 
+    
     it "should have the content 'ATS.WEB'" do
-      visit root_path
       page.should have_content('ATS.WEB')
     end
     it "should have the base title" do
-      visit root_path
       page.should have_selector('title',
                         :text => "ATS.WEB")
     end
     it "should not have the right title" do
-      visit root_path
       page.should_not have_selector('title', :text => "ATS.WEB | Home")
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit help_path
       page.should have_content('Help')
     end
     it "should have the right title" do
-      visit help_path
       page.should have_selector('title', :text => "ATS.WEB | Help")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the h1 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       page.should have_selector('title',
                     :text => "ATS.WEB | Contact")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About ATS'" do
-      visit about_path
+     before { visit about_path }
+   it "should have the content 'About ATS'" do
       page.should have_content('About ATS')
     end
     it "should have the right title" do
-      visit about_path
       page.should have_selector('title', :text => "ATS.WEB | About")
     end
   end
